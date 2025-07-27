e6faa3929c3c0d2fee86103920c12919245f14e3
ucctn <chetan.singhal@ucertify.com>
2025-07-27 13:47:36 -0600

    Merge branch 'main' of https://github.com/EnriqueSimonII/CIS406 into main

--- orderEntryPhase1/src/orderEntryPhase1/OrderEntryPhase1.java
+++ orderEntryPhase1/src/orderEntryPhase1/OrderEntryPhase1.java
@@ -1,11 +1,20 @@
 // Enrique Simon CIS406 Order Entry Phase 1
 
 package orderEntryPhase1;
+import java.util.Scanner;
 
 public class OrderEntryPhase1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		String testChar = "";
+		boolean testValid = false;
+		while (!testValid) {
+			System.out.print("Enter some Data: ");
+			testChar = scanner.nextLine();
+		}
 
 	}
 
