cbbcf63b497fc8529c279b7a355110b2f2f49a80
ucctn <chetan.singhal@ucertify.com>
2025-07-27 12:22:13 -0600

    Vacation Diary 1

---
+++ vacationDiary1/src/vacationDiary1/VacationDiary1.java
@@ -0,0 +1,15 @@
+// Enrique J Simon CIS406 Vacation Diary 1
+
+package vacationDiary1;
+import java.util.Scanner;
+
+public class VacationDiary1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Vacation Diary\n\n");
+
+	}
+
+}
