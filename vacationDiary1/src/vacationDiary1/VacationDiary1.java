9fb13471c1a12a74f857cc386fac5e7459efc8d2
ucctn <chetan.singhal@ucertify.com>
2025-07-27 12:34:50 -0600

    Vacation Diary 1

--- vacationDiary1/src/vacationDiary1/VacationDiary1.java
+++ vacationDiary1/src/vacationDiary1/VacationDiary1.java
@@ -9,7 +9,28 @@ public class VacationDiary1 {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Vacation Diary\n\n");
-
+		
+		// Collect Data
+		System.out.print("Enter Date Vacation Started (mm/dd/yyyy): ");
+		String startDate = scanner.nextLine();
+		System.out.print("Enter City Visted: ");
+		String cityVisted = scanner.nextLine();
+		System.out.print("Enter COuntry Visted: ");
+		String countryVisted = scanner.nextLine();
+		System.out.print("Enter number of days: ");
+		String numDays = scanner.nextLine();
+		System.out.print("Enter mode of travel (car, airplane, ship, train, bus): ");
+		String modeTravel = scanner.nextLine();
+		
+		// Print Data
+		System.out.println("Details of your trip:\n");
+		System.out.print("Date: " + startDate);
+		System.out.print("City: " + cityVisted);
+		System.out.print("Country: " + countryVisted);
+		System.out.print("Number of Days: " + numDays);
+		System.out.print("Traveled By: " + modeTravel);
+		System.out.print("Bye!!!");
+		
 	}
 
 }
